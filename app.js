3dce20c34c0ed365d2b0ac5b6a4a954b30e3ff45
dc-rubiano-rojas <dcrubiano02@gmail.com>
2024-07-24 09:33:00 -0500

    :sparkles: new commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// Supports ES6
+// import { create, Whatsapp } from 'venom-bot';
+const venom = require('venom-bot');
+
+venom
+    .create({
+        session: 'session-name', //name of session
+        multidevice: false // for version not multidevice use false.(default: true)
+    })
+    .then((client) => start(client))
+    .catch((erro) => {
+        console.log(erro);
+    });
+
+function start(client) {
+    client.onMessage((message) => {
+        client
+            .sendText(message.from, message.body)
+            .then((result) => {
+                console.log('Result: ', result); //return object success
+            })
+            .catch((erro) => {
+                console.error('Error when sending: ', erro); //return object error
+            });
+    });
+}
